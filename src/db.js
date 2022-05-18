ebf9ca661202316439a5aa134b8e642f17143b75
Suruchi Kumari <85888177+Epicsuru@users.noreply.github.com>
2022-05-18 19:03:14 +0530

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,1 +1,15 @@
+import firebase from "firebase/app";
+import "firebase/database";
 
+const config= {
+    //API KEYS
+    apiKey: "AIzaSyChwPRSDMrcuDazqVTFLJB_EguZ35iOguM",
+    authDomain: "firevuechat-2d7d9.firebaseapp.com",
+    projectId: "firevuechat-2d7d9",
+    storageBucket: "firevuechat-2d7d9.appspot.com",
+    messagingSenderId: "338415407393",
+    appId: "1:338415407393:web:24ea4bed17e38ea0ef6e60"
+}
+
+const db =firebase.initializeApp(config);
+export default db; 
